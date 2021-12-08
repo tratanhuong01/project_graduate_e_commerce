448ccfb47f85d8dec4c0ddf63225576851f84eba
Trà Hưởng <tratanhuong01@gmail.com>
2021-12-08 09:49:32 +0700

    fix http

--- src/reducers/socket.js
+++ src/reducers/socket.js
@@ -1,6 +1,7 @@
 import socketIOClient from "socket.io-client";
+import { SERVER_NODE } from "../constants/Config";
 
-const initialState = socketIOClient.connect("http://localhost:4444");
+const initialState = socketIOClient.connect(SERVER_NODE);
 
 const myReducer = (state = initialState, action) => {
   switch (action.type) {
